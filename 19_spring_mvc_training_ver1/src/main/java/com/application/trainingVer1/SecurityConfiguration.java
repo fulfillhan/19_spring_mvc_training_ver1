84d21da8a025b5fd9f443c43ca8771b14fb27df9
sanghui <tkdgml719@gamil.com>
2024-02-19 13:52:33 +0900

    연습 커밋

--- 19_spring_mvc_training_ver1/src/main/java/com/application/trainingVer1/SecurityConfiguration.java
+++ 19_spring_mvc_training_ver1/src/main/java/com/application/trainingVer1/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.application.trainingVer1;
 
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
